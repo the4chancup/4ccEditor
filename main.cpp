9dc4caf19dc83b346d6d0cba0e0886d535065229
Kiyose-san <kiyosevgl@gmail.com>
2024-04-15 00:27:17 +0200

    Removed PES16/17 specific FPC settings

--- main.cpp
+++ main.cpp
@@ -3893,13 +3893,10 @@ void team_fpc_on()
 			i_fpcBoot = 38;
 			i_fpcGkGlove = 12;
 		}
-
-		if(giPesVersion<18) SendDlgItemMessage(ghw_tab2, IDC_PHYS_SKIN, CB_SETCURSEL, (WPARAM)7, 0);
-		SendDlgItemMessage(ghw_tab2, IDC_STRP_WRTA, CB_SETCURSEL, (WPARAM)0, 0);
-		if( SendDlgItemMessage(ghw_main, IDC_PLAY_RPOS, CB_GETCURSEL, 0, 0) && giPesVersion<18 ) //GK sleeves are different in pre-Fox versions
-			SendDlgItemMessage(ghw_tab2, IDC_STRP_SLEE, CB_SETCURSEL, (WPARAM)1, 0);
-		else
-			SendDlgItemMessage(ghw_tab2, IDC_STRP_SLEE, CB_SETCURSEL, (WPARAM)2, 0);
+		
+		
+		SendDlgItemMessage(ghw_tab2, IDC_STRP_WRTA, CB_SETCURSEL, (WPARAM)0, 0);		
+		SendDlgItemMessage(ghw_tab2, IDC_STRP_SLEE, CB_SETCURSEL, (WPARAM)2, 0);
 		SendDlgItemMessage(ghw_tab2, IDC_STRP_SOCK, CB_SETCURSEL, (WPARAM)2, 0);
 		SendDlgItemMessage(ghw_tab2, IDC_STRP_TAIL, CB_SETCURSEL, (WPARAM)0, 0);
 		Button_SetCheck(GetDlgItem(ghw_tab2, IDB_STRP_ANTA),BST_UNCHECKED);
@@ -3929,28 +3926,23 @@ void team_fpc_on()
 								gplayers[jj].b_changed=true;
 							}
 
-							if(gplayers[jj].sleeve!=2 && (gplayers[jj].reg_pos!=0 || giPesVersion>=18))
+							if(gplayers[jj].sleeve!=2 && gplayers[jj].reg_pos!=0)
 							{
 								gplayers[jj].sleeve=2;
 								gplayers[jj].b_changed=true;
 							}
-							else if(gplayers[jj].sleeve!=1 && gplayers[jj].reg_pos==0 && giPesVersion<18)
+							else if(gplayers[jj].sleeve!=1 && gplayers[jj].reg_pos==0)
 							{
 								gplayers[jj].sleeve=1;
 								gplayers[jj].b_changed=true;
 							}
 
-							if(gplayers[jj].skin_col!=7 && giPesVersion<18)
-							{
-								gplayers[jj].skin_col=7;
-								gplayers[jj].b_changed=true;
-							}
-							if(gplayers[jj].boot_id!=i_fpcBoot && giPesVersion<19)
+							if(gplayers[jj].boot_id!=i_fpcBoot && giPesVersion<19 && gplayers[jj].boot_id==0)
 							{
 								gplayers[jj].boot_id=i_fpcBoot;
 								gplayers[jj].b_changed=true;
 							}
-							if(gplayers[jj].glove_id!=i_fpcGkGlove && giPesVersion<19)
+							if(gplayers[jj].glove_id!=i_fpcGkGlove && giPesVersion<19 && gplayers[jj].glove_id == 0)
 							{
 								gplayers[jj].glove_id=i_fpcGkGlove;
 								gplayers[jj].b_changed=true;
